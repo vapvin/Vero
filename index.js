297606dc35f681472dc12c33416aef9a7053a3cc
vapvin <tpehdj12@daum.net>
2020-05-30 09:56:15 +0900

    Using MiddleWares

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import logger from 'morgan';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 const app = express();
 
 const PORT = 4000
@@ -10,8 +12,11 @@ const handleListen = () =>console.log(`Listening On Port: http://localhost:${POR
 const homeRes =(req, res) =>res.send(`<h1>Hello, World</h1>`);
 const meRes = (req, res) => res.send("My Site");
 
-app.use(helmet())
-app.use(logger("dev"))
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend: true}));
+app.use(helmet());
+app.use(logger("dev"));
 
 app.get("/", homeRes);
 app.get("/me", meRes);
