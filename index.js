8f551074faaff77d5d6d83d12865bf879e912fe3
vapvin <tpehdj12@daum.net>
2020-05-30 22:01:28 +0900

    Two Pages

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const handleListen = () =>console.log(`Listening On Port: http://localhost:${POR
 
 const homeRes =(req, res) =>res.send(`<h1>Hello, World</h1>`);
 const meRes = (req, res) => res.send("My Site");
+const searchHandler = (req, res) => res.send("Search Pages");
+const signUp = (req, res) => res.send("Sign Up!!");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -20,4 +22,6 @@ app.use(logger("dev"));
 
 app.get("/", homeRes);
 app.get("/me", meRes);
+app.get("/search", searchHandler);
+app.get("/signup", signUp); // Change Post Method
 app.listen(PORT, handleListen);
