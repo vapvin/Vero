1849b0ab44011b17fe24aecec57adae85b59848e
vapvin <tpehdj12@daum.net>
2020-05-30 09:13:14 +0900

    Servers

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ const PORT = 4000
 function handleListen(){
     console.log(`Listening On Port: http://localhost:${PORT}`);
 }
+function homeRes(req, res){
+    console.log(req);
+    res.send(`<h1>Hello, World</h1>`);
+}
+function meRes(req, res){
+    res.send("My Site");
+}
+app.get("/", homeRes);
+app.get("/me", meRes);
 app.listen(PORT, handleListen);
