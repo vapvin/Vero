5aa564987b2b2ca1771ffcbf18bede42751a131d
vapvin <tpehdj12@daum.net>
2020-06-04 00:47:03 +0900

    Divide > Conquer

--- app.js
+++ app.js
@@ -5,14 +5,9 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 const app = express();
 
-const PORT = 4000
-
-const handleListen = () =>console.log(`Listening On Port: http://localhost:${PORT}`);
 
 const homeRes =(req, res) =>res.send(`<h1>Hello, World</h1>`);
 const meRes = (req, res) => res.send("My Site");
-const searchHandler = (req, res) => res.send("Search Pages");
-const signUp = (req, res) => res.send("Sign Up!!");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -22,6 +17,5 @@ app.use(logger("dev"));
 
 app.get("/", homeRes);
 app.get("/me", meRes);
-app.get("/search", searchHandler);
-app.get("/signup", signUp); // Change Post Method
-app.listen(PORT, handleListen);
+
+export default app;
