330aaa05d677504282a40370eee02063a8418b81
vapvin <tpehdj12@daum.net>
2020-06-10 06:28:08 +0900

    Completely

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,6 +1,9 @@
 export const main = (req, res) => res.render("main", {pageTitle: "Home"});
-export const search = (req, res) => res.render("search", {pageTitle: "Search"});
-export const videos = (req, res) => res.render("videos", {pageTitle: "Videos"});
+export const search = (req, res) => {
+    const {query: {term: searchingBy }} = req;
+    res.render("search", {pageTitle: "Search", searchingBy});
+};
+
 export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
