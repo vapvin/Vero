11b9db1bafe079cfe7a3b4edf0293717ce5a8e8d
vapvin <tpehdj12@daum.net>
2020-06-11 21:59:06 +0900

    Awesome Post Method

--- Router/mainRouter.js
+++ Router/mainRouter.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import routes from '../routes';
 import { main, search } from '../controller/videoController';
-import { sign, signIn, signOut } from '../controller/userController';
+import { getSign, postSign, signIn, signOut } from '../controller/userController';
 
 const mainRouter = express.Router();
 
 mainRouter.get(routes.main, main);
-mainRouter.get(routes.signup, sign);
+mainRouter.get(routes.signup, getSign);
+mainRouter.post(routes.signup, postSign);
 mainRouter.get(routes.signin, signIn);
 mainRouter.get(routes.signout, signOut);
 mainRouter.get(routes.search, search);
--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,11 @@
-export const sign = (req, res) => res.render("sign", {pageTitle: "Sign Up"});
+export const getSign = (req, res) => {
+    res.render("sign", {pageTitle: "Sign Up"});
+};
+
+export const postSign = (req, res) => {
+    console.log(req.body);
+    res.render("sign", {pageTitle: "Sign Up"});
+}
 export const signIn = (req, res) => res.render("signin", {pageTitle: "Sign In"});
 export const signOut = (req, res) => res.render("signout", {pageTitle: "Sign Out"});
 export const users = (req,res) => res.render("users", {pageTitle: "Users"});
