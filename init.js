38d3aa138a35dfa0b32c516c76948f998206a9eb
vapvin <tpehdj12@daum.net>
2020-07-02 22:08:26 +0900

    Push here test

--- init.js
+++ init.js
@@ -1,3 +1,4 @@
+require("dotenv");
 import app from './app';
 
 const PORT = 4000;
@@ -5,3 +6,7 @@ const PORT = 4000;
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
+
+// Test Code
+
+console.log(dotenv);
