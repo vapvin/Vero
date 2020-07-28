b2598375cce53acdd3939fe6dbcd05a473d65f42
vapvin <vins@vins.dev>
2020-07-29 00:23:37 +0900

    Next Dev Meta Classes

--- portfolios/admin.py
+++ portfolios/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from . import models
 
+@admin.register(models.ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+
+    pass
+
 @admin.register(models.PortFolioModel)
-class PostAdmin(admin.ModelAdmin):
+class PortFolioAdmin(admin.ModelAdmin):
 
     pass
--- portfolios/models.py
+++ portfolios/models.py
@@ -1,11 +1,34 @@
 from django.db import models
 from core import models as core_models
 
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+class ProductType(AbstractItem):
+
+    """ ProductType Model Definition """
+
+    pass
+
 class PortFolioModel(core_models.TimeStampedModel):
     """ Class Portfolio Definition """
 
     name = models.CharField(max_length=140)
     description = models.TextField(null=True)
+    product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True)
     skills = models.CharField(max_length=140)
     period = models.IntegerField(default=1)
     linked = models.URLField(default='')
+
+    def __str__(self):
+        return self.name
