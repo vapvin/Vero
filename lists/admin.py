fcdb9b6cb3d74d50f3938a9b1641574476ac640e
vapvin <tpehdj12@daum.net>
2020-08-02 17:03:27 +0900

    List Model and List Admin Definition

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Lists)
+class ListAdmin(admin.ModelAdmin):
+
+    """ List Admin Definition """
+
+    pass
--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+class Lists(core_models.TimeStampedModel):
+
+    """ List Model Definition """
+
+    name = models.CharField(max_length=140, blank=True)
+    portfolio = models.ManyToManyField("portfolios.PortFolioModel", blank=True)
+
+    def __str__(self):
+        return self.name
