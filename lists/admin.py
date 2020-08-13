fd05c8c3d6b0b0c2460ca3a9d61d71ef5d37045a
vapvin <vins@vins.dev>
2020-08-13 23:12:04 +0900

    Admin Settings

--- lists/admin.py
+++ lists/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.Lists)
-class ListAdmin(admin.ModelAdmin):
+@admin.register(models.PostLists)
+class PostListAdmin(admin.ModelAdmin):
 
     """ List Admin Definition """
 
     pass
+
+@admin.register(models.PortfolioLists)
+class PortfolioListAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "count_portfolio",
+    )
--- portfolios/admin.py
+++ portfolios/admin.py
@@ -4,9 +4,40 @@ from . import models
 @admin.register(models.ProductType)
 class ProductTypeAdmin(admin.ModelAdmin):
 
-    pass
+    list_display = ("name",)
 
 @admin.register(models.PortFolioModel)
 class PortFolioAdmin(admin.ModelAdmin):
 
+    list_display = (
+        "name",
+        "description",
+        "product_type",
+        "skills",
+        "period",
+        "linked",
+        "count_photos"
+    )
+
+    ordering = (
+        "name",
+        "product_type",
+        "skills"
+    )
+
+    list_filter = (
+        "product_type",
+        "skills"
+    )
+
+    search_fields = ("=skills", "^name")
+
+    def count_photos(self, obj):
+        return obj.photos.count()
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
     pass
+
