96a15548ac723a90b27615ab0e21ab17b8ae2c47
vapvin <tpehdj12@daum.net>
2020-08-17 22:38:04 +0900

    Initalizer

--- lists/admin.py
+++ lists/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.Lists)
-class ListAdmin(admin.ModelAdmin):
+@admin.register(models.PostLists)
+class PostListAdmin(admin.ModelAdmin):
+
+    """ List Admin Definition """
+
+    pass
+
+@admin.register(models.PortfolioLists)
+class PortfolioAdmin(admin.ModelAdmin):
 
     """ List Admin Definition """
 
